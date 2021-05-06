91797d3b0ed1164141f03bc6d44d0f5e08aeeb70
atchaw <garahsami@gmail.com>
2021-05-06 17:19:56 +0200

    create train module and update optimizer module

--- android_manifast/optimizer.py
+++ android_manifast/optimizer.py
@@ -1,9 +1,10 @@
 import torch.nn as nn
 import torch.optim as optim
 
-def getOptimizer(learningRate, weightDecay ):
+def getCriterion():
     # specify loss function (categorical cross-entropy)
     criterion = nn.NLLLoss()
 
+def getOptimizer(learningRate, weightDecay ):
     # specify optimizer
     optimizer = optim.Adam(model.parameters(), lr=learningRate, weight_decay=weightDecay)
---
+++ android_manifast/train.py
@@ -0,0 +1,34 @@
+import accuracy
+
+#define our training loop.
+def train(model, iterator, optimizer, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for (data, target) in iterator:
+        
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        
+        # clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # calculate accuracy
+        acc = accuracy.calculate_accuracy(output, target)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        
+        # update training loss and train accuracy
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
